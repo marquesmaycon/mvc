6733fc71aac86fa4f3a66f021d1225bc7d0f3016
henriquemaycon <mayconmarquesh@gmail.com>
2023-02-25 14:29:47 -0300

    starting project

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -8,13 +8,4 @@ class HomeController extends Controller {
     public function index() {
         $this->render('home', ['nome' => 'Bonieky']);
     }
-
-    public function sobre() {
-        $this->render('sobre');
-    }
-
-    public function sobreP($args) {
-        print_r($args);
-    }
-
 }
